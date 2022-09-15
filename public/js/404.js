44a8552d7ba1366ddd86168d47f224c7448b13bc
Petrus Matiros <petrus.matiros@gmail.com>
2022-09-15 17:38:58 +0200

    404.js creates a single setInterval, and not one every connection

--- public/js/404.js
+++ public/js/404.js
@@ -17,19 +17,17 @@ socket.on("connect", () => {
   animateRobot();
 });
 
+var theHeader = document.getElementById("header");
+var preFix = "Beep Boop";
+var robotFrames = ["└[∵]┘", "┌[∵]┐"];
+
+setInterval(() => {
+  var rand = random(0, robotFrames.length - 1);
+  theHeader.innerText = preFix + " " + robotFrames[rand];
+}, 850);
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function animateRobot() {
-  var theHeader = document.getElementById("header");
-  var preFix = "Beep Boop";
-  var robotFrames = ["└[∵]┘", "┌[∵]┐"];
-
-  setInterval(() => {
-    var rand = random(0, robotFrames.length - 1);
-    theHeader.innerText = preFix + " " + robotFrames[rand];
-  }, 850);
 }
 
 /**
